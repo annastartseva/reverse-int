01a9aa6c8c3e5cadb2da2b7ab53f55c02f29a54a
Anna Startseva <a.v.starceva@gmail.com>
2020-10-14 20:17:54 +0600

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let n_str = String(Math.abs(n));
+    let result = '';
+    //console.log('n: ' + n);
+    //console.log("n_str.length: " + n_str.length);
+    for (let i = 0; i < n_str.length; i++) {
+        //console.log("i: " + i);
+        //console.log('n_str[i]: ' + n_str[i]);
+        result = `${n_str[i]}${result}`;
+    }
+    return Number(result);
 }
